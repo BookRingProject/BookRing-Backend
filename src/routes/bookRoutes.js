2a81ba7f07ea89c9e578e03fa27b7aa41fb8ba03
BookRingProject <bookring9@gmail.com>
2026-04-29 06:53:42 -0700

    Update bookRoutes.js

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -8,11 +8,11 @@ const {
 } = require('../controllers/bookController');
 const { protect } = require('../middleware/authMiddleware');
 const { isLecturer } = require('../middleware/roleMiddleware');
-const { upload, handleUploadError } = require('../middleware/uploadMiddleware');
+const { uploadPDF, handleUploadError } = require('../middleware/uploadMiddleware');
 
 const router = express.Router();
 
-router.post('/upload', protect, isLecturer, upload.single('pdf'), handleUploadError, uploadBook);
+router.post('/upload', protect, isLecturer, uploadPDF.single('pdf'), handleUploadError, uploadBook);
 router.get('/', protect, getAllBooks);
 router.get('/my-books', protect, isLecturer, getMyBooks);
 router.get('/:id', protect, getBookById);
