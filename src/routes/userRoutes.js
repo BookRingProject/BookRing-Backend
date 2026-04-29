496b9a06f2141feffd9c39a863775b68ea9d5294
BookRingProject <bookring9@gmail.com>
2026-04-29 06:28:04 -0700

    Update userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const { uploadAvatar } = require('../controllers/uploadController');
 const { protect } = require('../middleware/authMiddleware');
-const { upload } = require('../middleware/uploadMiddleware');
+const { uploadImage } = require('../middleware/uploadMiddleware');
 
 const router = express.Router();
 
-router.post('/upload-avatar', protect, upload.single('avatar'), uploadAvatar);
+router.post('/upload-avatar', protect, uploadImage.single('avatar'), uploadAvatar);
 
 module.exports = router;
